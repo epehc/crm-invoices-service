fbd2b9aec794527bed0689ff299cfd43f92baa60
Jose Cardona <jpcardonamena@gmail.com>
2024-12-21 00:22:22 +0100

    added logging and improved error handling

--- src/controllers/facturaController.ts
+++ src/controllers/facturaController.ts
@@ -1,85 +1,146 @@
 import {Request, Response} from 'express';
 import Factura from '../models/factura';
+import logger from "../utils/logger";
+import {validationResult} from "express-validator";
 
 export const getAllFacturas = async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        logger.error('Error al asignar roles: ', errors);
+        res.status(400).json({ errors: errors.array() });
+        return
+    }
     try {
         const facturas = await Factura.findAll();
+        logger.info('Facturas fetched successfully');
         res.status(200).json(facturas);
     } catch (error) {
+        logger.error('Error al obtener facturas: ', error);
         res.status(500).json({error: 'Failed to fetch facturas'});
     }
 }
 
 export const getFacturaByFacturaId = async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        logger.error('Error al asignar roles: ', errors);
+        res.status(400).json({ errors: errors.array() });
+        return
+    }
     try {
         const {factura_id} = req.params;
         const factura = await Factura.findByPk(factura_id);
         if (factura) {
+            logger.info('Factura fetched successfully');
             res.status(200).json(factura);
         } else {
+            logger.error('Factura no encontrada');
             res.status(404).json({error: 'Factura not found'});
         }
     } catch (error) {
+        logger.error('Error al obtener factura: ', error);
         res.status(500).json({error: 'Failed to fetch factura'});
     }
 }
 
 export const getFacturasByClienteId = async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        logger.error('Error al asignar roles: ', errors);
+        res.status(400).json({ errors: errors.array() });
+        return
+    }
     try {
         const {cliente_id} = req.params;
         const facturas = await Factura.findAll({where: {cliente_id}});
+        logger.info('Facturas fetched successfully');
         res.status(200).json(facturas);
     } catch (error) {
+        logger.error('Error al obtener facturas: ', error);
         res.status(500).json({error: 'Failed to fetch facturas'});
     }
 }
 
 export const getFacturasByNit = async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        logger.error('Error al asignar roles: ', errors);
+        res.status(400).json({ errors: errors.array() });
+        return
+    }
     try{
         const {nit} = req.params;
         const facturas = await Factura.findAll({where: {nit}});
+        logger.info('Facturas fetched successfully');
         res.status(200).json(facturas);
     }
-    catch (errror){
+    catch (error){
+        logger.error('Error al obtener facturas: ', error);
         res.status(500).json({error: 'Failed to fetch facturas'});
     }
 }
 
 export const createFactura = async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        logger.error('Error al asignar roles: ', errors);
+        res.status(400).json({ errors: errors.array() });
+        return
+    }
     try {
         const factura = await Factura.create(req.body);
+        logger.info('Factura created successfully');
         res.status(201).json(factura);
     } catch (error) {
+        logger.error('Error al crear factura: ', error);
         res.status(500).json({error: 'Failed to create factura'});
     }
 }
 
 export const updateFactura = async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        logger.error('Error al asignar roles: ', errors);
+        res.status(400).json({ errors: errors.array() });
+        return
+    }
     try {
         const {factura_id} = req.params;
         const factura = await Factura.findByPk(factura_id);
         if (factura) {
             await factura.update(req.body);
+            logger.info(`Factura ${factura_id} updated successfully`);
             res.status(200).json(factura);
         } else {
+            logger.error('Factura no encontrada');
             res.status(404).json({error: 'Factura not found'});
         }
     } catch (error) {
+        logger.error('Error al actualizar factura: ', error);
         res.status(500).json({error: 'Failed to update factura'});
     }
 }
 
 export const deleteFactura = async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        logger.error('Error al asignar roles: ', errors);
+        res.status(400).json({ errors: errors.array() });
+        return
+    }
     try {
         const {factura_id} = req.params;
         const factura = await Factura.findByPk(factura_id);
         if (factura) {
             await factura.destroy();
+            logger.info(`Factura ${factura_id} deleted successfully`);
             res.status(204).send();
         } else {
+            logger.error('Factura no encontrada');
             res.status(404).json({error: 'Factura not found'});
         }
     } catch (error) {
+        logger.error('Error al eliminar factura: ', error);
         res.status(500).json({error: 'Failed to delete factura'});
     }
 }
--- src/controllers/pagoController.ts
+++ src/controllers/pagoController.ts
@@ -1,74 +1,127 @@
 import {Request, Response} from 'express';
 import Pago from '../models/pago';
+import logger from "../utils/logger";
+import {validationResult} from "express-validator";
 
 export const getAllPagos = async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        logger.error('Error al asignar roles: ', errors);
+        res.status(400).json({ errors: errors.array() });
+        return
+    }
     try {
         const pagos = await Pago.findAll();
+        logger.info('Pagos fetched successfully');
         res.status(200).json(pagos);
     } catch (error) {
+        logger.error('Error al obtener pagos: ', error);
         res.status(500).json({error: 'Failed to fetch pagos'});
     }
 }
 
 export const getPagoByPagoId = async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        logger.error('Error al asignar roles: ', errors);
+        res.status(400).json({ errors: errors.array() });
+        return
+    }
     try {
         const {pago_id} = req.params;
         const pago = await Pago.findByPk(pago_id);
         if (pago) {
+            logger.info('Pago fetched successfully');
             res.status(200).json(pago);
         } else {
+            logger.error('Pago no encontrado');
             res.status(404).json({error: 'Pago not found'});
         }
     } catch (error) {
+        logger.error('Error al obtener pago: ', error);
         res.status(500).json({error: 'Failed to fetch pago'});
     }
 }
 
 export const getPagosByFacturaId = async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        logger.error('Error al asignar roles: ', errors);
+        res.status(400).json({ errors: errors.array() });
+        return
+    }
     try {
         const {factura_id} = req.params;
         const pagos = await Pago.findAll({where: {factura_id}});
+        logger.info('Pagos fetched successfully');
         res.status(200).json(pagos);
     } catch (error) {
+        logger.error('Error al obtener pagos: ', error);
         res.status(500).json({error: 'Failed to fetch pagos'});
     }
 }
 
 export const createPago = async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        logger.error('Error al asignar roles: ', errors);
+        res.status(400).json({ errors: errors.array() });
+        return
+    }
     try {
         const pago = await Pago.create(req.body);
+        logger.info('Pago creado exitosamente');
         res.status(201).json(pago);
     } catch (error) {
+        logger.error('Error al crear pago: ', error);
         res.status(500).json({error: 'Failed to create pago'});
     }
 }
 
 export const updatePago = async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        logger.error('Error al asignar roles: ', errors);
+        res.status(400).json({ errors: errors.array() });
+        return
+    }
     try {
         const {pago_id} = req.params;
         const pago = await Pago.findByPk(pago_id);
         if (pago) {
             await pago.update(req.body);
+            logger.info(`Pago ${pago_id} updated successfully`);
             res.status(200).json(pago);
         } else {
+            logger.error('Pago no encontrado');
             res.status(404).json({error: 'Pago not found'});
         }
     } catch (error) {
+        logger.error('Error al actualizar pago: ', error);
         res.status(500).json({error: 'Failed to update pago'});
     }
 }
 
 export const deletePago = async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        logger.error('Error al asignar roles: ', errors);
+        res.status(400).json({ errors: errors.array() });
+        return
+    }
     try {
         const {pago_id} = req.params;
         const pago = await Pago.findByPk(pago_id);
         if (pago) {
             await pago.destroy();
+            logger.info(`Pago ${pago_id} eliminado exitosamente`);
             res.status(204).json();
         } else {
+            logger.error('Pago no encontrado');
             res.status(404).json({error: 'Pago not found'});
         }
     } catch (error) {
+        logger.error('Error al eliminar pago: ', error);
         res.status(500).json({error: 'Failed to delete pago'});
     }
 }
