2b3ed0a4503cbeaffb7ff14e9a353a2d2298010e
Jose Cardona <jpcardonamena@gmail.com>
2024-12-21 00:21:57 +0100

    created setup for logging and swagger documentation

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import express from "express";
 import bodyParser from "body-parser";
 import facturaRoutes from "./routes/facturaRoutes";
 import pagoRoutes from "./routes/pagoRoutes";
+import {setupSwagger} from "./utils/swagger";
 
 
 const app = express();
 app.use(bodyParser.json());
+setupSwagger(app);
 app.use('/facturas', facturaRoutes);
 app.use('/pagos', pagoRoutes)
 
