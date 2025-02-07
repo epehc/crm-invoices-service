52004f99addae22e575f01fa2bfeb2e80817f7d4
Jose Cardona <jpcardonamena@gmail.com>
2025-02-07 15:35:56 +0100

    added code to force sync database (for troubleshooting, not to use in production)

--- src/database/db.ts
+++ src/database/db.ts
@@ -14,4 +14,20 @@ const sequelize = new Sequelize(
     }
 );
 
+//To force the database to sync with the models every time the server starts, uncomment the following code
+
+/* const syncDB = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Database connected");
+
+    await sequelize.sync({ force: true }); 
+    console.log("Database synchronized");
+  } catch (error) {
+    console.error("Error syncing database:", error);
+  }
+};
+
+syncDB(); */
+
 export default sequelize
