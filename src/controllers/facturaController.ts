248a52137cd41435ed179ed204e25454704b36a4
Jose Cardona <jpcardonamena@gmail.com>
2025-02-07 10:41:49 +0100

    added fetching with pagination and search

--- src/controllers/facturaController.ts
+++ src/controllers/facturaController.ts
@@ -2,11 +2,13 @@ import {Request, Response} from 'express';
 import Factura from '../models/factura';
 import logger from "../utils/logger";
 import {validationResult} from "express-validator";
+import { Op } from 'sequelize';
+
 
 export const getAllFacturas = async (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        logger.error('Error al asignar roles: ', errors);
+        logger.error('Error al cargar facturas: ', errors);
         res.status(400).json({ errors: errors.array() });
         return
     }
@@ -20,10 +22,54 @@ export const getAllFacturas = async (req: Request, res: Response) => {
     }
 }
 
+
+export const getFacturas = async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        logger.error('Error al cargar facturas: ', errors);
+        res.status(400).json({ errors: errors.array() });
+        return
+    }
+    try {
+        const page = parseInt(req.query.page as string) || 1;
+        const pageSize = parseInt(req.query.pageSize as string) || 12;
+        
+        const offset = (page - 1) * pageSize;
+
+        const query = req.query.query ? req.query.query.toString() : '';
+        console.log("Query: ", req.query);
+
+        const whereClause = query ? {
+            [Op.or]: [
+                { cliente_nombre: { [Op.iLike]: `%${query}%` } },
+                { estado: { [Op.iLike]: `%${query}%` } },
+                { nit: { [Op.iLike]: `%${query}%` } },
+                { fecha: { [Op.iLike]: `%${query}%` } },
+            ]
+        } : {};
+
+        const {count, rows: facturas} = await Factura.findAndCountAll({
+            where: whereClause,
+            offset,
+            limit: pageSize
+        });
+        logger.info('Facturas fetched successfully');
+        res.status(200).json({
+            data: facturas,
+            total: count,
+            totalPages: Math.ceil(count / pageSize),
+            currentPage: page
+        });
+    } catch (error) {
+        logger.error('Error al obtener facturas: ', error);
+        res.status(500).json({error: 'Failed to fetch facturas'});
+    }
+}
+
 export const getFacturaByFacturaId = async (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        logger.error('Error al asignar roles: ', errors);
+        logger.error('Error al cargar factura: ', errors);
         res.status(400).json({ errors: errors.array() });
         return
     }
@@ -46,13 +92,13 @@ export const getFacturaByFacturaId = async (req: Request, res: Response) => {
 export const getFacturasByClienteId = async (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        logger.error('Error al asignar roles: ', errors);
+        logger.error('Error al cargar las facturas del cliente: ', errors);
         res.status(400).json({ errors: errors.array() });
         return
     }
     try {
-        const {cliente_id} = req.params;
-        const facturas = await Factura.findAll({where: {cliente_id}});
+        const client_id = req.params.client_id;
+        const facturas = await Factura.findAll({where: {client_id}});
         logger.info('Facturas fetched successfully');
         res.status(200).json(facturas);
     } catch (error) {
@@ -64,7 +110,7 @@ export const getFacturasByClienteId = async (req: Request, res: Response) => {
 export const getFacturasByNit = async (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        logger.error('Error al asignar roles: ', errors);
+        logger.error('Error al cargar las facturas asociadas con el nit: ', errors);
         res.status(400).json({ errors: errors.array() });
         return
     }
@@ -83,7 +129,7 @@ export const getFacturasByNit = async (req: Request, res: Response) => {
 export const createFactura = async (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        logger.error('Error al asignar roles: ', errors);
+        logger.error('Error al generar factura: ', errors.array());
         res.status(400).json({ errors: errors.array() });
         return
     }
@@ -100,7 +146,7 @@ export const createFactura = async (req: Request, res: Response) => {
 export const updateFactura = async (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        logger.error('Error al asignar roles: ', errors);
+        logger.error('Error al actualizar factura: ', errors);
         res.status(400).json({ errors: errors.array() });
         return
     }
@@ -124,7 +170,7 @@ export const updateFactura = async (req: Request, res: Response) => {
 export const deleteFactura = async (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        logger.error('Error al asignar roles: ', errors);
+        logger.error('Error al borrar roles: ', errors);
         res.status(400).json({ errors: errors.array() });
         return
     }
@@ -146,6 +192,12 @@ export const deleteFactura = async (req: Request, res: Response) => {
 }
 
 export const getLatestFacturas = async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        logger.error('Error al cargar facturas: ', errors);
+        res.status(400).json({ errors: errors.array() });
+        return
+    }
     try {
         const facturas = await Factura.findAll({order: [['fecha', 'DESC']], limit: 10});
         logger.info('Facturas fetched successfully');
@@ -153,5 +205,30 @@ export const getLatestFacturas = async (req: Request, res: Response) => {
     } catch (error) {
         logger.error('Error al obtener facturas: ', error);
         res.status(500).json({error: 'Failed to fetch facturas'});
+    }
+}
+
+export const anularFactura = async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        logger.error('Error al anular factura: ', errors);
+        res.status(400).json({ errors: errors.array() });
+        return
+    }
+    try{
+        const {factura_id} = req.params;
+        const factura = await Factura.findByPk(factura_id);
+        if (factura){
+            factura.estado = 'anulada';
+            await factura.save();
+            logger.info('Factura anulada exitosamente');
+            res.status(200).json(factura);
+        } else {
+            logger.error('Factura no encontrada');
+            res.status(404).json({error: 'Factura not found'});
+        }
+    } catch (error){
+        logger.error('Error al anular factura: ', error);
+        res.status(500).json({error: 'Failed to anular factura'});
     }
 }
