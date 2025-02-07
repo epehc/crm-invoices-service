37eebf3cf2353c7b7783a3258cf3093d018a20ad
Jose Cardona <jpcardonamena@gmail.com>
2025-02-07 10:40:15 +0100

    Added cors

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,16 @@ import bodyParser from "body-parser";
 import facturaRoutes from "./routes/facturaRoutes";
 import pagoRoutes from "./routes/pagoRoutes";
 import {setupSwagger} from "./utils/swagger";
-
+import cors from "cors";
 
 const app = express();
+
+const corsOptions = {
+    origin: process.env.FRONTEND_URL, // Your frontend's URL
+    credentials: true, // Allow credentials (cookies, headers)
+};
+
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 setupSwagger(app);
 app.use('/facturas', facturaRoutes);
