4ec0d488296ba6fb87e3ffb689a0ee2f3327c10f
Jose Cardona <jpcardonamena@gmail.com>
2025-02-07 15:35:23 +0100

    improved payment processing logic

--- src/controllers/pagoController.ts
+++ src/controllers/pagoController.ts
@@ -2,6 +2,9 @@ import {Request, Response} from 'express';
 import Pago from '../models/pago';
 import logger from "../utils/logger";
 import {validationResult} from "express-validator";
+import Factura from '../models/factura';
+import {v4 as uuidv4} from 'uuid';
+
 
 export const getAllPagos = async (req: Request, res: Response) => {
     const errors = validationResult(req);
@@ -51,13 +54,32 @@ export const getPagosByFacturaId = async (req: Request, res: Response) => {
         return
     }
     try {
-        const {factura_id} = req.params;
+        const factura_id = req.params.factura_id;
         const pagos = await Pago.findAll({where: {factura_id}});
         logger.info('Pagos fetched successfully');
         res.status(200).json(pagos);
     } catch (error) {
         logger.error('Error al obtener pagos: ', error);
         res.status(500).json({error: 'Failed to fetch pagos'});
+    }
+}
+
+export const procesarPago = async (factura_id: number, monto: number) => {
+    try {
+        const factura = await Factura.findByPk(factura_id);
+        if (factura) {
+            factura.abonado += monto;
+            factura.saldo_pendiente -= monto;
+            if(factura.saldo_pendiente <= 0) {
+                factura.estado = 'pagada';
+            }
+            await factura.save();
+            logger.info(`Factura ${factura_id} updated successfully`);
+        } else {
+            logger.error('Factura no encontrada');
+        }
+    } catch (error) {
+        logger.error('Error al procesar pago: ', error);
     }
 }
 
@@ -69,8 +91,16 @@ export const createPago = async (req: Request, res: Response) => {
         return
     }
     try {
-        const pago = await Pago.create(req.body);
-        logger.info('Pago creado exitosamente');
+        const {factura_id, fecha, monto, boleta_pago} = req.body;
+
+        const pago = await Pago.create({
+            pago_id: uuidv4(),
+            factura_id,
+            fecha,
+            monto,
+            boleta_pago
+        });
+        logger.info('Pago creado y procesado exitosamente');
         res.status(201).json(pago);
     } catch (error) {
         logger.error('Error al crear pago: ', error);
@@ -126,6 +156,3 @@ export const deletePago = async (req: Request, res: Response) => {
     }
 }
 
-export const procesarPago = async (req: Request, res: Response) => {
-    const {factura_id, monto} = req.body;
-}
